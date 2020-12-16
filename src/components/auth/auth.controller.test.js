fb8812787913a142b4a9c47fc4ce179c68ec4ba0
Vighnesh Raut <vighnesh.raut13@gmail.com>
2020-12-16 10:04:51 +0530

    add auth controller tests

---
+++ src/components/auth/auth.controller.test.js
@@ -0,0 +1,53 @@
+describe('Assign correct middlewares to routes', () => {
+  let router;
+  let middlewares;
+  let authMiddlewares;
+
+  beforeAll(() => {
+    /* eslint-disable global-require */
+    middlewares = require('../../middlewares');
+    authMiddlewares = require('./auth.middlewares');
+
+    jest.mock('express', () => ({
+      Router() {
+        return {
+          get: jest.fn(),
+          use: jest.fn(),
+        };
+      },
+    }));
+    router = require('./auth.controller');
+    /* eslint-enable global-require */
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should define 3 routes', () => {
+    expect(router.get).toBeCalledTimes(3);
+  });
+
+  it('should assign correct middlewares to /auth/github', () => {
+    expect(router.get).toBeCalledWith(
+      '/github',
+      authMiddlewares.passportGithubAuth,
+    );
+  });
+
+  it('should assign correct middlewares to /auth/github/callback', () => {
+    expect(router.get).toBeCalledWith(
+      '/github/callback',
+      authMiddlewares.passportGithubAuthCallback,
+      authMiddlewares.githubSignupSuccess,
+    );
+  });
+
+  it('should assign correct middlewares to /auth/verify', () => {
+    expect(router.get).toBeCalledWith(
+      '/verify',
+      middlewares.isLoggedIn,
+      authMiddlewares.verifyLoginSuccess,
+    );
+  });
+});
