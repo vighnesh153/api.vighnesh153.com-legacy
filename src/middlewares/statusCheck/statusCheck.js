979b19778922d95059cc499c200b655ec01d7032
Vighnesh Raut <vighnesh.raut13@gmail.com>
2021-01-04 22:54:31 +0530

    rm unnecessary log

--- src/middlewares/statusCheck/statusCheck.js
+++ src/middlewares/statusCheck/statusCheck.js
@@ -1,8 +1,4 @@
 module.exports = function statusCheck(req, res) {
-  const { logger } = req;
-
-  logger.info({ message: 'Status Check' });
-
   // TODO: Health checkup, more sophisticated analysis
   res.sendStatus(200);
 };
--- src/middlewares/statusCheck/statusCheck.test.js
+++ src/middlewares/statusCheck/statusCheck.test.js
@@ -1,26 +1,20 @@
 const statusCheck = require('./statusCheck');
 
 describe('Middleware: Status Check', () => {
-  let reqStub;
   let resStub;
   beforeEach(() => {
-    reqStub = {
-      logger: {
-        info: () => {},
-      },
-    };
     resStub = {
       sendStatus: jest.fn(),
     };
   });
 
   it('should invoke sendStatus once', () => {
-    statusCheck(reqStub, resStub);
+    statusCheck({}, resStub);
     expect(resStub.sendStatus).toBeCalledTimes(1);
   });
 
   it('should send a status of 200', () => {
-    statusCheck(reqStub, resStub);
+    statusCheck({}, resStub);
     expect(resStub.sendStatus).toBeCalledWith(200);
   });
 });
