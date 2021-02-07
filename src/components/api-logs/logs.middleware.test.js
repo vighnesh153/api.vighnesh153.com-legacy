018cf53d9108e8f02f04a95ea09e20f60b6c240f
Vighnesh Raut <vighnesh.raut13@gmail.com>
2021-02-07 19:26:34 +0530

    add logs.middleware.test

---
+++ src/components/api-logs/logs.middleware.test.js
@@ -0,0 +1,36 @@
+describe('Logs Middleware tests', () => {
+  let mockServices;
+  let reqStub;
+  let resStub;
+
+  let logsMiddleware;
+
+  beforeEach(() => {
+    jest.resetModules();
+
+    reqStub = { logger: { info: jest.fn() } };
+    resStub = { json: jest.fn() };
+    mockServices = { something: Math.random() };
+
+    jest.mock('./logs.service', () => ({
+      getUniqueLogServices: jest.fn().mockResolvedValue(mockServices),
+    }));
+
+    // eslint-disable-next-line global-require
+    logsMiddleware = require('./logs.middleware');
+  });
+
+  it('should return correct response when unique services are requested', async () => {
+    await logsMiddleware.getUniqueLogServices(reqStub, resStub);
+
+    expect(resStub.json).toBeCalledWith(mockServices);
+  });
+
+  it('should return cool message when invoking catchAllWildcardRouteHandler', () => {
+    logsMiddleware.catchAllWildcardRouteHandler(reqStub, resStub);
+
+    expect(resStub.json).toBeCalledWith({
+      message: '✨ Logs Component root for logs.vighnesh153.com ✨',
+    });
+  });
+});
