33b14752218c7b9852a591a6e9790a56bd107184
Vighnesh Raut <vighnesh.raut13@gmail.com>
2021-02-07 19:28:23 +0530

    remove unnecessary service

--- src/components/api-logs/logs.service.js
+++ src/components/api-logs/logs.service.js
@@ -12,16 +12,3 @@ exports.getUniqueLogServices = async function getUniqueLogServices(logger) {
     return null;
   }
 };
-
-exports.getLogs = async function getLogs(logger) {
-  const LogModel = mongoose.model('Log');
-  try {
-    return await LogModel.find().exec();
-  } catch (err) {
-    logger.error({
-      message: err.message,
-      stackTrace: err.stack,
-    });
-    return null;
-  }
-};
