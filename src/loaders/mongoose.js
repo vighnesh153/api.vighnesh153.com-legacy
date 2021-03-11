e74c09cea8405c79af62fe19d80f3c50402452ca
Vighnesh Raut <vighnesh.raut13@gmail.com>
2021-03-11 08:20:39 +0530

    set pool size for the connection

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -4,6 +4,7 @@ const config = require('../config');
 
 async function configureMongoose() {
   await mongoose.connect(config.MONGODB_URI, {
+    poolSize: 5,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
